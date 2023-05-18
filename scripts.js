3affb53fe6df7b807c05ff4aa0022a4d01861ef3
mord66 <sercangunbatti@gmail.com>
2023-05-18 19:06:35 +0300

    Create starting grid box

--- scripts.js
+++ scripts.js
@@ -1,12 +1,25 @@
 const container = document.getElementById("container");
+for (i=1; i<=256; i++){
+    let div = document.createElement("div");
+    div.classList.add("box");
+    container.appendChild(div);
+
+}
+
 
 function makeGrid(num){
     container.style.setProperty("grid-template-columns", "repeat(" + num + ", 1fr)");
-    for (i=1; i<=num*num; i++){
-        let div = document.createElement("div");
-        div.classList.add("box");
-        container.appendChild(div);
-
+    if (num<=100){
+        for (i=1; i<=num*num; i++){
+            let div = document.createElement("div");
+            div.classList.add("box");
+            container.appendChild(div);
+                }
+        
+    }
+    else {
+        alert("Number too big!");
     }
 }
-makeGrid(7);
+
+
