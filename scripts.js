ea0f4f6c5844cc68e8d7f59dcdbad3d5405e33fa
mord66 <sercangunbatti@gmail.com>
2023-05-18 18:48:02 +0300

    Complete grid creater basen on input from user

--- scripts.js
+++ scripts.js
@@ -1,18 +1,12 @@
 const container = document.getElementById("container");
 
 function makeGrid(num){
-    for (i=1; i<=num; i++){
-        const row = document.createElement("div");
-        row.classList.add("box");
-        container.appendChild(row);
+    container.style.setProperty("grid-template-columns", "repeat(" + num + ", 1fr)");
+    for (i=1; i<=num*num; i++){
+        let div = document.createElement("div");
+        div.classList.add("box");
+        container.appendChild(div);
 
-        for (j=1; j<num; j++){
-            const col = document.createElement("div");
-            col.classList.add("box");
-            row.appendChild(col);
-        }
     }
 }
-
-
-makeGrid(4);
+makeGrid(7);
