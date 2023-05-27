5e3268535ff8633198927892be739809d9ac3efa
mord66 <sercangunbatti@gmail.com>
2023-05-27 18:03:39 +0300

    Complete project

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ const blackButton = document.querySelector(".black");
 const whiteButton = document.querySelector(".white");
 const colorButton = document.querySelector(".color");
 const randomButton = document.querySelector(".random");
-const color ="";
+const colorPicker = document.getElementById("colorPicker");
 
 // Add clicked effect to buttons //
 
@@ -30,25 +30,49 @@ function clickedFunction(){
 }
 // Add clicked effect to buttons //
 
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
 
 function makeGrid(num){
     container.style.setProperty("grid-template-columns", "repeat(" + num + ", 1fr)");
     container.innerHTML = ""; // Clear previous grid
         for (i=1; i<=num*num; i++){
             let div = document.createElement("div");
-            div.addEventListener("mouseover", function(){
-                if(blackButton.classList.contains("clicked")){
-                    this.style.backgroundColor = "black";
-                }
-                else if (whiteButton.classList.contains("clicked")){
-                    this.style.backgroundColor = "white";
-                }
-            })
+            div.addEventListener("mouseover", colorGrid)
             div.classList.add("box");
             container.appendChild(div);
                 }
         
 }
+
+
+
+
+function colorGrid(){
+    if(blackButton.classList.contains("clicked")){
+        this.style.backgroundColor = "black";
+    }
+    else if (whiteButton.classList.contains("clicked")){
+        this.style.backgroundColor = "white";
+    }
+    else if (randomButton.classList.contains("clicked")){
+        var r = getRndInteger(1, 255);
+        var g = getRndInteger(1, 255);
+        var b = getRndInteger(1, 255);
+        this.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+    }
+    else if (colorButton.classList.contains("clicked")){
+        this.style.backgroundColor = colorPicker.value;
+}
+}
+
+var chosenColor="";
+colorPicker.addEventListener("input", function(){
+    chosenColor = colorPicker.value
+})
+
+
 
 slider.addEventListener("input", function() {
     const num = parseInt(slider.value);
